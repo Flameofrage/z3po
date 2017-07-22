7b352dc11127d1e692c86fac50396bd22de964a9
Lance Zeligman <lance@zeligman.net>
2017-07-22 19:03:11 -0400

    Avoiding copyright infringement

--- modules/R2Z2status.rb
+++ modules/R2Z2status.rb
@@ -1,6 +1,6 @@
 module Z3PO
   Z3PO.presence(from: "R2Z2", status: :online) do |event|
-        event.bot.channel(313009155969384448).send_message('There you are R2D2, you shouldn\'t go running off like that.')
+        event.bot.channel(313009155969384448).send_message('There you are R2Z2, you shouldn\'t go running off like that.')
   end
 
   Z3PO.presence(from: "R2Z2", status: :offline) do |event|
