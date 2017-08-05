e2a6646f78229079ea00d195a5af3ec092ad9fc7
Lance Zeligman <lance@zeligman.net>
2017-08-04 23:20:11 -0400

    Fixed typos v2

--- modules/grepping.rb
+++ modules/grepping.rb
@@ -8,12 +8,12 @@ module Z3PO
         Z3PO.send_message(313009155969384448, "!musichelp")
   end
 
-	Z3P0.message(with_text: "that's my fetish") do |event|
+	Z3PO.message(with_text: "that's my fetish") do |event|
 		num = Random.rand(2)
 			if num == 1
-				Z3P0.send_message(event.channel, "<@#{event.user.id}: http://i.imgur.com/7PyvoJu.jpg")
+				Z3PO.send_message(event.channel, "<@#{event.user.id}: http://i.imgur.com/7PyvoJu.jpg")
 			else
-				Z3P0.send_message(event.channel, "<@#{event.user.id}: http://i.imgur.com/VOBdI06.jpg")
+				Z3PO.send_message(event.channel, "<@#{event.user.id}: http://i.imgur.com/VOBdI06.jpg")
 			end
 	end
 end
