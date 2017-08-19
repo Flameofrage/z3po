11b52e058ca72a0280d13f7b2b3deea57a51ff28
Lance Zeligman <lance@zeligman.net>
2017-08-19 03:43:08 -0400

    the monica addition

--- modules/grepping.rb
+++ modules/grepping.rb
@@ -1,13 +1,17 @@
 module Z3PO
   Z3PO.message(with_text: "4chan") do |event|
-        Z3PO.send_temporary_message(event.channel, "https://www.youtube.com/watch?v=GoRPVsN2SVM", 15)
+		Z3PO.send_temporary_message(event.channel, "https://www.youtube.com/watch?v=GoRPVsN2SVM", 15)
   end
 
   Z3PO.message(with_text: "music help") do |event|
-        Z3PO.send_message(313009155969384448, "<@#{event.user.id}>, please allow me")
-        Z3PO.send_message(313009155969384448, "!musichelp")
+		Z3PO.send_message(313009155969384448, "<@#{event.user.id}>, please allow me")
+		Z3PO.send_message(313009155969384448, "!musichelp")
   end
 
+  Z3PO.message(with_text: "monica") do |event|
+		Z3PO.send_message(event.channel, "https://i.imgur.com/ord2XEU.jpg")
+  end
+	
 	Z3PO.message(with_text: /that's my fetish/i) do |event|
 		num = Random.rand(2)
 			if num == 1
