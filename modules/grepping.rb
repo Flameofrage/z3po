35187aa7bc650447043ffbf105ac4da715d8e95c
lanceofwhichwedream <lance.zeligman@gmail.com>
2018-01-31 15:13:25 -0500

    Updating greetings

--- modules/grepping.rb
+++ modules/grepping.rb
@@ -1,50 +1,46 @@
-module Z3PO
-  Z3PO.message(contains: /4chan/i) do |event|
+Z3PO.message(contains: /4chan/i) do |event|
 		Z3PO.send_temporary_message(event.channel, "https://www.youtube.com/watch?v=GoRPVsN2SVM", 15)
-  end
-  
-  Z3PO.message(contains: /what flavor is it/i) do |event|
-    Z3PO.send_message(event.channel, "Boot to the head!")
-  end
-
-  Z3PO.message(contains: /kill me/i) do |event|
-    Z3PO.send_message(event.channel, "<@#{event.user.id}>, :bang: :gun: You're dead")
-  end
-
-  Z3PO.message(contains: /music help/i) do |event|
-		Z3PO.send_message(313009155969384448, "<@#{event.user.id}>, please allow me")
-		Z3PO.send_message(313009155969384448, "!musichelp")
-  end
-
-  Z3PO.message(contains: /Currently, yuki_nyiaya is streaming/i) do |event|
-    replace = event.message.content
-    event.message.delete
-    replace.sub!("yuki_nyiaya", "❄YUKI❄")
-    Z3PO.send_message(event.channel, replace)
-  end
-
-  Z3PO.message(contains: /monica/i) do |event|
-		Z3PO.send_message(event.channel, "https://i.imgur.com/ord2XEU.jpg")
-  end
-	
-  Z3PO.message(contains: /sunscreen/i) do |event|
-    Z3PO.send_message(event.channel, "SPF 100? I hope it's in stock, cause I rub it on my nips and the bell end of my cock!")
-  end
-	
-  Z3PO.message(contains: /that's my fetish/i) do |event|
-		num = Random.rand(2)
-			if num == 1
-				event.message.delete
-				Z3PO.send_message(event.channel, "#{event.user.username} meant http://i.imgur.com/7PyvoJu.jpg")
-			else
-				event.message.delete
-				Z3PO.send_message(event.channel, "#{event.user.username} meant http://i.imgur.com/VOBdI06.jpg")
-			end
-	end
-
-  Z3PO.message(contains: /what time is it/i) do |event|
-    Z3PO.send_message(event.channel, "<@#{event.user.id}>, http://i.imgur.com/5WUBQOd.gif")
-  end
+end
+ 
+Z3PO.message(contains: /what flavor is it/i) do |event|
+  Z3PO.send_message(event.channel, "Boot to the head!")
 end
 
+Z3PO.message(contains: /kill me/i) do |event|
+  Z3PO.send_message(event.channel, "<@#{event.user.id}>, :bang: :gun: You're dead")
+end
 
+Z3PO.message(contains: /music help/i) do |event|
+		Z3PO.send_message(313009155969384448, "<@#{event.user.id}>, please allow me")
+		Z3PO.send_message(313009155969384448, "!musichelp")
+end
+
+Z3PO.message(contains: /Currently, yuki_nyiaya is streaming/i) do |event|
+  replace = event.message.content
+  event.message.delete
+  replace.sub!("yuki_nyiaya", "❄YUKI❄")
+  Z3PO.send_message(event.channel, replace)
+end
+
+Z3PO.message(contains: /monica/i) do |event|
+		Z3PO.send_message(event.channel, "https://i.imgur.com/ord2XEU.jpg")
+end
+
+Z3PO.message(contains: /sunscreen/i) do |event|
+  Z3PO.send_message(event.channel, "SPF 100? I hope it's in stock, cause I rub it on my nips and the bell end of my cock!")
+end
+
+Z3PO.message(contains: /that's my fetish/i) do |event|
+	num = Random.rand(2)
+		if num == 1
+			event.message.delete
+			Z3PO.send_message(event.channel, "#{event.user.username} meant http://i.imgur.com/7PyvoJu.jpg")
+		else
+			event.message.delete
+			Z3PO.send_message(event.channel, "#{event.user.username} meant http://i.imgur.com/VOBdI06.jpg")
+		end
+end
+
+Z3PO.message(contains: /what time is it/i) do |event|
+  Z3PO.send_message(event.channel, "<@#{event.user.id}>, http://i.imgur.com/5WUBQOd.gif")
+end
--- z3po.rb
+++ z3po.rb
@@ -1,20 +1,19 @@
 require 'discordrb'
 require 'yaml'
-module Z3PO
-  # Bot Config file
-  CONFIG = YAML.load_file('data/config.yaml')
+require 'sinatra'
+require 'haml'
+include FileUtils::Verbose
+# Bot Config file
+CONFIG = YAML.load_file('data/config.yaml')
 
-  # Bot Config
-  Z3PO = Discordrb::Bot.new(token: CONFIG['token'],
-			  client_id: CONFIG['client_id'],
-			  ignore_bots: false)
+# Bot Config
+Z3PO = Discordrb::Bot.new(token: CONFIG['token'],
+			                    client_id: CONFIG['client_id'],
+			                    ignore_bots: false)
 
-  #Z3po Modules
-  Dir['modules/*.rb'].each { |mod| require_relative mod; puts "Loaded: #{mod}" }
+#Z3po Modules
+Dir['modules/*.rb'].each { |mod| require_relative mod; puts "Loaded: #{mod}" }
 
-  #Aysnc
-  Z3PO.run :async
+#Aysnc
+Z3PO.run :async
 
-#  Z3PO.send_temporary_message(289603265677492245, 'How do you do, I am Z3PO, Human Cyborg Relations. How might I serve you?', 10)
-  Z3PO.sync
-end
