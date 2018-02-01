a96edf485746f139aadb58383ea54bff0cdc221d
lanceofwhichwedream <lance.zeligman@gmail.com>
2018-02-01 00:41:59 -0500

    Role automation, I hope

--- modules/greetings.rb
+++ modules/greetings.rb
@@ -4,6 +4,8 @@ Z3PO.message(with_text: ["Hey z3po!","Hey 3po!"]) do |event|
 end
 
  Z3PO.member_join do |event|
+      member = event.user.id
+      member.add_role(405529215036030987)
       event.bot.channel(408353405933977610).send_message("Greetings <@#{event.user.id}>! I am Z3po, human cyborg relations. Welcome! This is R2Z2, he can do a number of functions which you can instruct him to do with !help. Please allow me to demonstrate for you.")
       Z3PO.send_message(408353405933977610, "!help")
-end 
+end
